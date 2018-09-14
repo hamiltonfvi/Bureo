c99ab89c6e20bc4dc64f966e2220211d2bdae6e9
Hamilton Jimenez <hamiltonjbeach7@gmail.com>
2018-09-14 16:51:39 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/bureo/InformationFragment.java
@@ -0,0 +1,29 @@
+package com.example.android.bureo;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class InformationFragment extends Fragment {
+
+
+    public InformationFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_information, container, false);
+        return rootView;
+    }
+
+}
---
+++ app/src/main/java/com/example/android/bureo/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.android.bureo;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mDrawer;
+    private Toolbar mToolbar;
+    private ActionBarDrawerToggle mToggle;
+    private int mSelectedId;
+    private boolean mDrawerIsSeen = false;
+    private boolean isStartup = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        mDrawer = findViewById(R.id.main_drawer);
+        mDrawer.setNavigationItemSelectedListener(this);
+
+        mDrawerLayout = findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        if(!drawerSeen()){
+
+            showDrawer();
+
+        } else{
+
+            hideDrawer();
+        }
+
+    }
+}
