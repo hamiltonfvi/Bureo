abbbe8d9c2b7d17c7e9b69f8e12b1bf53dfde2da
Hamilton Jimenez <hamiltonjbeach7@gmail.com>
2018-09-19 16:13:37 -0400

    -initial commit

--- app/src/main/java/com/example/android/bureo/MainActivity.java
+++ app/src/main/java/com/example/android/bureo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.bureo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initiate UI components
+        final TextView explore = findViewById(R.id.explore);
+
+        //Set OnClickListeners on clickable TextViews
+        explore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), ArtFragment.class);
+                startActivity(i);
+            }
+        });
     }
 }
 
